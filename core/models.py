6da01e5cdae37495f4cff7931e01a41b472d0817
Will <wtprattstephen@quinnipiac.edu>
2015-09-24 18:37:09 +0000

    Add questions to admin panel

--- core/models.py
+++ core/models.py
@@ -5,3 +5,6 @@ class Question(models.Model):
   title = models.CharField(max_length=300)
   description = models.TextField(null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
+  
+  def __unicode__(self):
+      return self.title
